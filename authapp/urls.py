cd5acf6869d89d55fdb9d5f48295cfd8da95a872
Oleksandr Shtyrov <shtyrov89@gmail.com>
2021-02-04 17:18:14 +0200

    Исправление ошибки в urls.py

--- authapp/urls.py
+++ authapp/urls.py
@@ -5,8 +5,8 @@ from authapp.views import login, register, logout
 app_name = 'authapp'
 
 urlpatterns = [
-    path('/login', login, name='login'),
-    path('/register', register, name='register'),
-    path('/logout', logout, name='logout'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout, name='logout'),
 
 ]
