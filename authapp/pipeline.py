d0928d9df8c3adfa9e2628d8776a64cd07e02b98
Oleksandr Shtyrov <shtyrov89@gmail.com>
2021-03-28 21:12:14 +0300

    profile.html fixed.

--- authapp/pipeline.py
+++ authapp/pipeline.py
@@ -1,4 +1,3 @@
-
 from collections import OrderedDict
 from datetime import datetime
 from urllib.parse import urlencode, urlunparse
@@ -13,6 +12,18 @@ from authapp.models import ShopUserProfile
 def save_user_profile(backend, user, response, *args, **kwargs):
     if backend.name != 'vk-oauth2':
         return
+
+    # print(response)
+    """ response
+    {'access_token': 'aa8e63b982e3a5dc60', 
+    'expires_in': 86400, 'user_id': 111111, 
+    'email': 'aaaa@bbb.ru', 'first_name': 'name', 'id': 11111, 'last_name': 'surname', 
+    'screen_name': 'soloninin_anton', 'nickname': '', 
+    'photo': 'url', 'user_photo': 'url'}
+    """
+
+    # access_token = response['access_token']
+    # api_url = f"https://api.vk.com/method/users.get/?fields=bdate,about,sex&access_token={access_token}&v=5.92"
 
     api_url = urlunparse(('https',
                           'api.vk.com',
@@ -29,7 +40,13 @@ def save_user_profile(backend, user, response, *args, **kwargs):
         return
 
     data = resp.json()
+    # print(data)
+    """ data = 
+    {'response': [{'first_name': 'name', 'id': 1111, 'last_name': 'sur name', 
+    'can_access_closed': True, 'is_closed': False, 'sex': 2, 'bdate': '23.02.2000', 'about': '1'}]}
+    """
     data = data['response'][0]
+
     user.email = response['email']
     if data['sex']:
         if data['sex'] == 2:
